7dac7337413ffc371692d22ecff1506b60166ebf
katamartin <katasm@gmail.com>
2023-10-11 15:27:06 -0400

    Update deps and set up pre-commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Layout, Row, Column } from '@carbonplan/components'
 const Index = () => {
   return (
     <Layout>
-      <Row sx={{fontSize: [4, 5, 6, 7], my: [5, 6, 7, 8]}}>
+      <Row sx={{ fontSize: [4, 5, 6, 7], my: [5, 6, 7, 8] }}>
         <Column start={[1, 2, 2, 2]} width={[6]}>
           This is a sample site
         </Column>
